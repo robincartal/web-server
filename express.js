dccf78340e0f8afab915c2ceb1e881695980e312
Robin CARTAL <robin.cartal@gmail.com>
2016-12-28 09:38:32 +0100

    1st commit

---
+++ express.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var app = express()
+var port = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log(new Date().toISOString() + " ### Request: " + req.method + " " + req.originalUrl);
+		next();
+	}
+}
+
+app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/about', function (req, res) {
+  res.send('About')
+})
+
+app.use( express.static(__dirname + "/public") );
+console.log(__dirname);
+ 
+app.listen(port, function() {
+	console.log( "Server started on port " + port + "!" )
+})
